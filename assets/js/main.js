4c9c34c375d2f002a68b5392281d32f9db386e26
Amish Verma <amishhverma@gmail.com>
2022-12-01 20:46:50 +0530

    add scroll to top functionality

--- assets/js/main.js
+++ assets/js/main.js
@@ -152,3 +152,12 @@ function scrollHeader() {
   else nav.classList.remove("scroll-header");
 }
 window.addEventListener("scroll", scrollHeader);
+
+// Show scroll up
+function scrollUp() {
+  const scrollUp = document.getElementById("scroll-up");
+  // When the scroll is higher than 560 viewport height, add the show-scroll class to the a tag with the scroll-top class
+  if (this.scrollY >= 560) scrollUp.classList.add("show-scroll");
+  else scrollUp.classList.remove("show-scroll");
+}
+window.addEventListener("scroll", scrollUp);
